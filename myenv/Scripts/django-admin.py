977746d599fd1f54238256e2e4d5c495f111bed7
Afra yasmeen <ay20aai@herts.ac.uk>
2023-04-06 13:24:45 +0100

    First commit

---
+++ myenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\imafr\desktop\2. mug\epredictor\letsgo\django-prjct\myenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
