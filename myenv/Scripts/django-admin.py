a38fe717abbfefc44900b6ab0bd5e0dc177f1275
Afra yasmeen <ay20aai@herts.ac.uk>
2023-04-06 15:51:29 +0100

    Revert "First commit"
    This reverts commit 977746d599fd1f54238256e2e4d5c495f111bed7.

--- myenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\imafr\desktop\2. mug\epredictor\letsgo\django-prjct\myenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
